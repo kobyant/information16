5e0ae79a8cb68b4156acf7386353f2624ac5952f
heJing <jinghedeveloper@126.com>
2018-10-14 15:59:17 +0800

    日志信息的集成

--- manager.py
+++ manager.py
@@ -15,7 +15,8 @@
 6.数据库迁移配置
 
 """""
-
+import logging
+from flask import current_app
 from info import create_app
 
 #调用业务模块获取app
@@ -32,6 +33,18 @@ def hello_world():
     #测试session,存取数据
     # session["age"] = "13"
     # print(session.get("age"))
+
+    #使用loggin日志模块输出内容
+    logging.debug("调试信息1")
+    logging.info("详细信息1")
+    logging.warning("警告信息1")
+    logging.error("错误信息1")
+
+    #上面的方式可以使用current_app输出,在控制台输出的时候有华丽分割线,写入到文件是一样的
+    # current_app.logger.debug("调试信息2")
+    # current_app.logger.info("详细信息2")
+    # current_app.logger.warning("警告信息2")
+    # current_app.logger.error("错误信息2")
 
     return "helloworld100"
 
