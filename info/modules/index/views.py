0f90e64c7d38a080135709c83f061db73926751f
heJing <jinghedeveloper@126.com>
2018-10-14 17:53:20 +0800

    首页页面渲染

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,5 +1,6 @@
 from info import redis_store
 from . import index_blu
+from flask import render_template
 
 @index_blu.route('/')
 def hello_world():
@@ -24,4 +25,4 @@ def hello_world():
     # current_app.logger.warning("警告信息2")
     # current_app.logger.error("错误信息2")
 
-    return "helloworld100"
+    return render_template("news/index.html")
