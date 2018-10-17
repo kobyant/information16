3088662a3571a7e4a2ce6842e79668a398aa4681
heJing <jinghedeveloper@126.com>
2018-10-17 17:38:02 +0800

    新闻详情页数据获取

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,5 +1,8 @@
+from info.models import News
+from info.utils.response_code import RET
 from . import news_blue
-from flask import render_template
+from flask import render_template, current_app, jsonify, abort
+
 
 # 获取新闻详情
 # 请求路径: /news/<int:news_id>
@@ -9,4 +12,20 @@ from flask import render_template
 @news_blue.route('/<int:news_id>')
 def news_detail(news_id):
 
-    return render_template("news/detail.html")
+    #1.根据新闻编号获取,新闻对象
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="新闻获取失败")
+
+    #2.判断新闻对象是否存在,后续会对404做统一处理
+    if not news:
+        abort(404)
+
+    #3.携带新闻数据,到模板页面显示
+    data = {
+        "news":news.to_dict()
+    }
+
+    return render_template("news/detail.html",data=data)
