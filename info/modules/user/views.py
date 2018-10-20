736123016f9f9112baa28ac64ae930d21ba11c51
heJing <jinghedeveloper@126.com>
2018-10-20 17:34:24 +0800

    用户新闻发布列表获取

--- info/modules/user/views.py
+++ info/modules/user/views.py
@@ -6,6 +6,58 @@ from info.utils.response_code import RET
 from . import user_blue
 from flask import render_template, g, redirect, request, jsonify, current_app
 
+#功能描述: 获取新闻发布列表
+# 请求路径: /user/news_list
+# 请求方式:GET
+# 请求参数:p
+# 返回值:GET渲染user_news_list.html页面
+@user_blue.route('/news_list')
+@user_login_data
+def news_list():
+    """
+    - 1.获取参数
+    - 2.参数类型转换
+    - 3.分页查询
+    - 4.获取分页对象属性,总页数,当前页,当前页对象列表
+    - 5.对象列表转成字典列表
+    - 6.携带数据渲染页面
+    :return: 
+    """
+    # - 1.获取参数
+    page = request.args.get("p","1")
+    
+    # - 2.参数类型转换
+    try:
+        page = int(page)
+    except Exception as e:
+        page = 1
+        
+    # - 3.分页查询
+    try:
+        paginate =  News.query.filter(News.user_id == g.user.id).order_by(News.create_time.desc()).paginate(page,10,False)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="获取新闻失败")
+    
+    # - 4.获取分页对象属性,总页数,当前页,当前页对象列表
+    totalPage = paginate.pages
+    currentPage = paginate.page
+    items = paginate.items
+
+    # - 5.对象列表转成字典列表
+    news_list = []
+    for news in items:
+        news_list.append(news.to_review_dict())
+
+    # - 6.携带数据渲染页面
+    data = {
+        "totalPage":totalPage,
+        "currentPage":currentPage,
+        "news_list":news_list
+    }
+    return render_template("news/user_news_list.html",data=data)
+
+
 # 功能描述: 新闻发布
 # 请求路径: /user/news_release
 # 请求方式:GET,POST
